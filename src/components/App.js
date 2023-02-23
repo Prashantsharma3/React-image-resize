0ca4e1ba6e8f96291bc64b1b6d3f6caba9afe618
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-23 20:44:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import image from '../image.jpeg';
 import '../styles/App.css';
 
 const App = () => {
+  const [height, setHeight] = React.useState(320);
+const [width, setWidth] = React.useState(320);
   return (
     <div id="main">
-
-    </div>
+     <img id="resizable-img" src={image} height={height} width={width} />
+     </div>
+   <div>
+  <label htmlFor="width-slider">Width:</label>
+  <input id="width-slider" type="range" min="100" max="800" value={width} 
+         onChange={(e) => setWidth(parseInt(e.target.value))} />
+</div>
   )
 }
 
