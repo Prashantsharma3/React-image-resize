ee03ae619929f9f4b4c9455649cc8b64d7ace8bc
Prashant Sharma <116828172+Prashantsharma3@users.noreply.github.com>
2023-03-03 22:39:31 +0530

    Resize

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
 import image from '../image.jpeg';
 import '../styles/App.css';
 
 const App = () => {
   const [height, setHeight] = React.useState(320);
-const [width, setWidth] = React.useState(320);
+  const [width, setWidth] = React.useState(320);
+
   return (
     <div id="main">
       <img id="resizable-img" src={image} height={height} width={width} />
-    
-    
-        <label htmlFor="width-slider">Width:</label>
-        <input id="width-slider" type="range" min="100" max="800" value={width}
-          onChange={(e) => setWidth(parseInt(e.target.value))} />
-      </div>
-  )
-}
 
+      <label htmlFor="width-slider">Width:</label>
+      <input id="width-slider" type="range" min="100" max="800" value={width}
+        onChange={(e) => setWidth(parseInt(e.target.value))} />
+
+      <label htmlFor="height-slider">Height:</label>
+      <input id="height-slider" type="range" min="100" max="800" value={height}
+        onChange={(e) => setHeight(parseInt(e.target.value))} />
+    </div>
+  );
+};
 
 export default App;
+
